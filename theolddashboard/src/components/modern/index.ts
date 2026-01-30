3aae1b839580db1c95829fcab9ec07511c4f76be
smile lemon <muhamsyaddad@gmail.com>
2026-01-30 13:44:34 +0700

    fix the auth

--- theolddashboard/src/components/modern/index.ts
+++
@@ -1,6 +0,0 @@
-// Modern Component Exports
-export { ModernKPICard } from './ModernKPICard';
-export { ModernAnalyticsCard } from './ModernAnalyticsCard';
-export { ModernStatsCard } from './ModernStatsCard';
-export { ModernInsightsCard } from './ModernInsightsCard';
-export { ModernProgressCard } from './ModernProgressCard';
